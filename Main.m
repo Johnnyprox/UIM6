4d50c864a52b59df81ef79d503a33566dea7bc2d
Johnnyprox <janko.sevcja@gmail.com>
2021-12-09 10:28:48 +0100

    Final output

--- Main.m
+++ Main.m
@@ -4,7 +4,7 @@ function [ mae, mse, rmse, targetSeries, predictedSeries ] = Main( filePath )
 %nutné dodržet sktrukturu tohoto kódu. 
 
 %Vstup:     filePath:           Název složky (textový øetìzec) obsahující data
-filePath = pwd
+
 %Výstup:    mae:                 Støední absolutní chyba modelu
 %           mse:                 Støední kvadratická chyba modelu
 %           rmse:                Smìrodatná odchylka residuí
@@ -20,32 +20,70 @@ filePath = pwd
 
 
     %% Nastavení cest a inicializace promìnných
+% Doba bìhu    
+    tic 
+% Cesta k souboru
+    filePath = pwd; 
+
     if ~isfolder( filePath )
         error('Folder does not exist.')
     end
+% Naètení datového souboru          
+    inputData = readtable([ filePath '/' 'dataOH.csv' ]); 
+% Dle zadání rok <= 2006
+    inputData = inputData(inputData.Year <= 2006,:); 
+% Unikátní názvy týmù
+    Teams = table(unique(inputData.NOC));   
+% Unikátní hodnoty pro roky
+    Years = unique(inputData.Year);    
+% Délka hodnot promìnné Teams     
+    [numberRecords_teams, ~] = size(Teams);
+% Délka hodnot promìnné Years 
+    numberRecords_years = length(Years);
+% Pomocná promìnná tabulky
+    P = zeros(numberRecords_years,numberRecords_teams); 
+    T = zeros(numberRecords_years,numberRecords_teams); 
+% Pomocná promìnná pro poèítání poètu celkových medailí    
+    sum_target = 0; 
+% Pomocná promìnná pro poèítání poètu predikovaných medailí
+    sum_predicted = 0; 
+    
+
+% For cyklus pro poèítání jednotlivých predikcí zvláš pro každý rok a tým 
+    for y = 1:numberRecords_years
+        data_year = inputData(inputData.Year == Years(y),:);        
+        for t = 1:numberRecords_teams
+            team_value = string(Teams.Var1);
+            data_year_team = data_year(data_year.NOC == team_value(t),:);
+            if isempty(data_year_team)
+                continue
+            else          
+    
+            %% Urèení výstupu modelu pro 1 objekt
+                [ targetSeries, predictedSeries ] = MyModel( data_year_team ); %Výstup modelu (predikce poètu medaili) 
+
+                sum_target = sum_target + targetSeries;
+                sum_predicted = sum_predicted + predictedSeries;
+                P(y,t) = predictedSeries;
+                T(y,t) = targetSeries;
+
         
-    inputData = readtable([ filePath '/' 'dataOH.csv' ]); %Naètení datového souboru 
-    numberRecords = size( inputData, 1 ); % Zmìnit dle nutnosti  
+            %% Vyhodnocení modelu
 
-    myModelData = MyModel(inputData);
-  
-
-    
-
-    
-%     for idx = 1:numberRecords
-%         
-%     
-%         %% Urèení výstupu modelu pro 1 objekt
-%         [ targetSeries, predictedSeries ] = MyModel( inputData ); %Výstup modelu (predikce poètu medaili)
-% 
-%         
-%         %% Vyhodnocení modelu
-%         [ mae, mse, rmse ] = GetScoreOH( targetSeries, predictedSeries );
-
-%     end
-
-
+                [ mae, mse, rmse ] = GetScoreOH( targetSeries, predictedSeries );           
+           
+            end
+        end
+    end
+% Uložení tabulky predikovaných hodnot pro: Øádek = jednotlivé týmy, Sloupec = jednotlivé roky
+    writetable(array2table(P),'Prediction_series.csv', 'Delimiter', ','); 
+    writetable(array2table(T),'Target_series.csv', 'Delimiter', ',');
+% Výpoèet MAE, MSE, RMSE pro celkový model zadaných hodnot
+    [ mae, mse, rmse ] = GetScoreOH( sum_target, sum_predicted );
+% Výpis výsledku
+    fprintf('Celkový poèet predikovaných medailí: %d Støední absolutní chyba modelu:  %d .\n',sum_predicted,mae)
+% Doba bìhu
+    toc
 end
 
 
--- sortingData.m
+++ sortingData.m
@@ -1,24 +1,17 @@
-function [Gold, Bronze, Silver, countFull] = sortingData(data, team, year)
+function [countFull] = sortingData(data)
 %sorting Data: function for sorting data
-%   
-
-date = data(data.Year == year,:);
-
-
-% teamS = data.Team;
-
-
-% TeamnameS = unique(teamS);
 
 
 
-DataTeamS = date(date.NOC == team,:);
+Teamnames = unique(data);
+
+y.medal_value(strcmpi(y.Medal, "Gold")) =  1;
+y.medal_value(strcmpi(y.Medal, "Silver")) =  1;
+y.medal_value(strcmpi(y.Medal, "Bronze")) =  1;
+y.season(strcmpi(y.Season, "Summer")) =  1;
 
 
-[Gold,x] = size(DataTeamS(DataTeamS.Medal == "Gold","ID"));
-[Silver,x] = size(DataTeamS(DataTeamS.Medal == "Silver","ID"));
-[Bronze,x] = size(DataTeamS(DataTeamS.Medal == "Bronze","ID")); 
-[countFull,x] = size(DataTeamS);
 
 
 end
+
